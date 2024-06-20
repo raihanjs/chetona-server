dd080edfd59852bf72f7761d3627abdc75b4bdeb
raihanjs <rayhangazi1024@gmail.com>
2024-06-21 01:34:40 +0600

    get single promotion by id api added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // ========================Middlewares========================
+app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 // ========================Middlewares========================
@@ -10,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to ChetonaProkashon Server");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rjtfnh6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,7 +38,15 @@ async function run() {
     });
     // get all promotions
     app.get("/promotions", async (req, res) => {
-      const result = await promotions.findOne();
+      const result = await promotions.find().toArray();
+      res.send(result);
+    });
+    // get single promotion by id
+    app.get("/promotions/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await promotions.findOne(query);
       res.send(result);
     });
   } finally {
