491f9a597272ffafa755b9aa78cba43c67311de0
raihanjs <rayhangazi1024@gmail.com>
2024-06-21 11:02:10 +0600

    vercel customization file added

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ async function run() {
     // get single promotion by id
     app.get("/promotions/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await promotions.findOne(query);
       res.send(result);
